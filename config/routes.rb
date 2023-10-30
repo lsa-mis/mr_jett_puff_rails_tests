5637e521c8c954019ca2d1a7dff037e3bc8fc233
Margarita Barvinok <brita@umich.edu>
2023-10-30 10:00:02 -0400

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'hello_world/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'hello_world#index'
 end
