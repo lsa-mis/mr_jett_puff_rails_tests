8ffc0c8d7c9b1e5af5563832cfc5a55c3635c865
Margarita Barvinok <brita@umich.edu>
2023-10-30 10:01:11 -0400

    add create user tests

---
+++ spec/create_user_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+# RSpec.describe 'create user', type: :system do
+#   describe 'index page' do
+#     it 'shows the right content' do
+#       visit root_path
+#       click_on "Sign in"
+#       click_on "Sign up"
+#       fill_in 'Email', with: "testuser@test.com"
+#       fill_in 'Password', with: "secretsecret"
+#       fill_in 'Password confirmation', with: "secretsecret"
+#       sleep(2)
+#       click_on "Sign up"
+#       sleep(inspection_time=2)
+
+#       expect(page).to have_content("Welcome testuser@test.com")
+#     end
+#   end
+# end
+
+# RSpec.describe 'create user', type: :system do
+#   describe 'index page' do
+#     it 'show password mismatch error' do
+#       visit root_path
+#       click_on "Sign in"
+#       click_on "Sign up"
+#       fill_in 'Email', with: "testuser@test.com"
+#       fill_in 'Password', with: "secretsecret"
+#       fill_in 'Password confirmation', with: "secret"
+#       sleep(2)
+#       click_on "Sign up"
+#       sleep(inspection_time=2)
+
+#       expect(page).to have_content("Password confirmation doesn't match Password")
+#     end
+#   end
+# end
+
+RSpec.describe 'create user', type: :system do
+  describe 'index page' do
+    it 'show email has already been taken"' do
+      @user = FactoryBot.create(:user)
+      visit root_path
+      click_on "Sign up"
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password
+      click_on "Sign up"
+      sleep(inspection_time=2)
+
+      expect(page).to have_content("Email has already been taken")
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by ENV['SHOW_BROWSER'] ? :selenium_chrome : :selenium_chrome_headless
   end
+
+  config.include Warden::Test::Helpers
+  
 end
