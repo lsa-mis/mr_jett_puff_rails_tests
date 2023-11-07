4af9ecb53aecef1eee1f7e065cf420a8933b998a
Margarita Barvinok <brita@umich.edu>
2023-11-07 15:04:42 -0500

    user FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,30 @@
+# FactoryBot.define do
+#   factory :user do
+#     first_name { Faker::Name.first_name }
+#     last_name { Faker::Name.last_name }
+#     email { Faker::Internet.email }
+#     password { Faker::Internet.password(min_length: 10) }
+
+#     factory :physician_user do
+#       role { 'physician' }
+#     end
+#   end
+# end
+
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 10) }
+    username { Faker::Internet.username }
+    password { Faker::Internet.password }
+
+    trait :with_name do
+      first_name { "John" }
+      last_name { "Smith" }
+    end
+
+    trait :with_phone do
+      phone { "(555) 555-5555" }
+    end
+
   end
 end
 
