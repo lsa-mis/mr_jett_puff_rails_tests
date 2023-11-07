b35fe2c11c775e71ecb2c228c307a53858d24d05
Margarita Barvinok <brita@umich.edu>
2023-11-07 14:10:10 -0500

    update User Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,18 @@
 FactoryBot.define do
   factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 10) }
   end
 end
+
+# FactoryBot.define do
+#   factory :user do
+#     first_name { 'John' }
+#     last_name { 'Smith' }
+#     # email { 'john.smith@example.com' }
+#     sequence(:email) { |n| "person#{n}@example.com" }
+#     password { Faker::Internet.password(min_length: 10) }
+#   end
+# end
